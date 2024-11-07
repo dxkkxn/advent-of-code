17057eeb35a798721db882dadedf3d53a6e7537f
Youssef Benjelloun <ybkstudies@gmail.com>
2024-11-07 18:40:13 +0100

    formated day 01

--- 2022/day-01/src/main.rs
+++ 2022/day-01/src/main.rs
@@ -7,10 +7,16 @@ fn main() -> Result<(), std::io::Error> {
     };
     let mut content = String::new();
     file.read_to_string(&mut content)?;
-    let v: Vec<Vec<i32>> = content.split("\n\n").map(|elf_bag| {
-        elf_bag.split('\n').map(|num| num.parse::<i32>().unwrap_or(0)).collect()
-    }).collect();
-                                           // collect::<Vec<&str>>();
+    let v: Vec<Vec<i32>> = content
+        .split("\n\n")
+        .map(|elf_bag| {
+            elf_bag
+                .split('\n')
+                .map(|num| num.parse::<i32>().unwrap_or(0))
+                .collect()
+        })
+        .collect();
+    // collect::<Vec<&str>>();
     // let v2: Vec<Vec<&str>> = v.iter().map(|bag| bag.split('\n').collect::<Vec<&str>>()).collect();
     // println!("v: {v2:?}");
     // let v3: Vec<Vec<i32>> = v2.iter().map(|row|
@@ -23,24 +29,29 @@ fn main() -> Result<(), std::io::Error> {
     //                               }).collect::<Vec<i32>>()
     //                           ).collect();
     // // let result:i32 = v3.iter().map(|bag| bag.iter().sum()).max().unwrap();
-    let result = v.iter().map(|bag| bag.iter().sum::<i32>()).collect::<Vec<i32>>()
-                          .iter().fold(vec![0, 0, 0], |mut acc, &num| {
-                if num > acc[0] {
-                    acc[2] = acc[1];
-                    acc[1] = acc[0];
-                    acc[0] = num;
-                } else if num > acc[1] {
-                    acc[2] = acc[1];
-                    acc[1] = num;
-                } else if num > acc[2] {
-                    acc[2] = num;
-                }
-                acc
-            }).iter().sum::<i32>();
+    let result = v
+        .iter()
+        .map(|bag| bag.iter().sum::<i32>())
+        .collect::<Vec<i32>>()
+        .iter()
+        .fold(vec![0, 0, 0], |mut acc, &num| {
+            if num > acc[0] {
+                acc[2] = acc[1];
+                acc[1] = acc[0];
+                acc[0] = num;
+            } else if num > acc[1] {
+                acc[2] = acc[1];
+                acc[1] = num;
+            } else if num > acc[2] {
+                acc[2] = num;
+            }
+            acc
+        })
+        .iter()
+        .sum::<i32>();
     // v.map(|bag| bag.split)
     println!("v: {v:?}");
     println!("{result:?}");
-
 
     Ok(())
 }
